65b39f902b9dfb44fdbd70667f3c3c77e1319dc4
Kit-kat1 <Katynichka95@mail.ru>
2015-11-02 18:33:54 +0200

    made websites page with list of all tracking websites. made method for inserting status of website to database

--- src/AppBundle/Controller/RoutingController.php
+++ src/AppBundle/Controller/RoutingController.php
@@ -31,9 +31,9 @@ class RoutingController extends Controller
                 $down++;
             }
         }
-        $track['up'] = $up;
-        $track['down'] = $down;
-        $track['websites'] = $websites;
+//        $track['up'] = $up;
+//        $track['down'] = $down;
+//        $track['websites'] = $websites;
 //        $status = json_encode($track);
 //        return $this->render('admin2/dashboard.html.twig', array('user' => $this->getUser(), 'track' => $status));
         return $this->render('admin2/dashboard.html.twig', array('user' => $this->getUser(), 'websites' => $websites,
@@ -54,5 +54,57 @@ class RoutingController extends Controller
             ->createQuery("SELECT u FROM AppBundle:Users u WHERE u.username != 'admin' ")
             ->getResult();
         return $this->render('admin2/admin.html.twig', array('users' => $users, 'user' => $this->getUser()));
+    }
+
+    /**
+     * @Route("/dashboard/websites", name="websites")
+     */
+    public function allWebsitesAction()
+    {
+        $web = $this->getDoctrine()->getRepository('AppBundle:Websites')
+            ->findAll();
+        $checks = $this->get('app.pingdom_connect')->connect();
+
+        $webStatus = [];
+        foreach ($checks as $check) {
+            $webStatus[] = array($check['hostname'] => $check['status']);
+        }
+
+        $url = [];
+        foreach ($checks as $check) {
+            $url[] = $check['hostname'];
+        }
+
+
+        $webUrl = [];
+        foreach ($web as $website) {
+            $webUrl[] = $website->getUrl();
+        }
+
+        $diff = array_diff($webUrl, $url);
+        $cross = array_uintersect($url, $webUrl, "strcasecmp");
+
+        foreach ($diff as $url) {
+            $site = $this->getDoctrine()->getRepository('AppBundle:Websites')
+                ->findOneBy(array('url' => $url));
+            $site->setStatus();
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($site);
+            $em->flush();
+        }
+
+        foreach ($webStatus as $siteStatus) {
+            $url = array_keys($siteStatus);
+            if (in_array($url[0], $cross)) {
+                $site = $this->getDoctrine()->getRepository('AppBundle:Websites')
+                    ->findOneBy(array('url' => $url[0]));
+                $status = array_values($siteStatus);
+                $site->setStatus($status[0]);
+                $em = $this->getDoctrine()->getManager();
+                $em->persist($site);
+                $em->flush();
+            }
+        }
+        return $this->render('admin2/websites.html.twig', array('websites' => $web));
     }
 }
